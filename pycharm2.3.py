9ffbcb974ac3aeb74f5c34226e84f998d9fc074e
ronitimonen <ronitim@metropolia.fi>
2023-01-10 12:06:02 +0200

    Initial commit

---
+++ pycharm2.3.py
@@ -0,0 +1,5 @@
+kanta = float(input("Suorakulmion kanta (cm)"))
+korkeus = float(input("Suorakulmion korkeus (cm)"))
+
+print("Suorakulmion piiri: " ,2*kanta+2*korkeus)
+print("Suorakulmion pinta-ala: " , kanta*korkeus, "Cm2")
